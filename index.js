31b3e212fe15969abb7efbbdb9f316d908614a16
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-07-24 14:30:55 -0400

    added in store items, and started working on store functions. Added save and load button. added click tree

--- index.js
+++ index.js
@@ -1,10 +1,82 @@
 let score = 0;
-let money = 0;
+let autoClick = 0;
+// let clicksPerSecond 
+
+function update(){
+    document.getElementById("score").value= score;
+}
+function timer(){
+    score = score + autoClick;
+    update()
+
+}
+
+setInterval(timer,1000)
 
 function add(){
     score = score + 1
-    money = money + 1
-    document.getElementById("score").value= score
+    update()    
+}
+
+function save(){
+    localStorage.setItem("score", score);
+}
+function load(){
+    score = localStorage.getItem("score");
+    score = parseInt(score);
+    update()
+}
+
+function buyLumberjack(){
+    if(score >= ((autoClick+1)^8)){
+        score = score - ((autoClick+1)^8);
+        autoClick = autoClick + 1;
+        update()
+    
+    }
+}
+
+function buyIronAxe(){
+    if(score >= 100){
+        score = score + 2;
+        update();
+        
+    }
     
 }
+function buyStoneAxe(){
+    if(score >= 1000){
+        score = score + 5;
+        update();
+    }
+}
+function buyDoubleAxe(){
+    
+}
+function buyGoldAxe(){
+    
+}
+function buyHolyAxe(){
+    
+}
+function buyPaulAxe(){
+    
+}
+function buyHouse(){
+        
+}
+function buyFarm(){
+        
+}
+function buyBlacksmith(){
+    
+}
+function buySawmill(){
+    
+}
+function buyWizard(){
+    
+}
+
+
 
